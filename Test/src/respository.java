26e0e70bb4f09e44e0e95cd58f677ebde6a84324
Zoe Developer <zoe@dev.org>
2013-07-09 06:34:16 -0400

    Commit
    Signed-off-by: Zoe Developer <zoe@dev.org>

--- Test/src/respository.java
+++ Test/src/respository.java
@@ -13,34 +13,29 @@ import java.io.FileWriter;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.revwalk.RevCommit;
 public class respository {
-
-
-
-
-//commit 一次的事情
-//回滚，提交
     public static void main(String[] args) throws Throwable {
         File root = new File("gitme");
         if(!root.exists())
             root.mkdir();
         File gitF = new File("gitme/.git");
-        if(!gitF.exists()) {//如果已经初始化过,那肯定有.git文件夹
-                        //初始化git库,相当于命令行的 git init
+        if(!gitF.exists()) {
+        	//如果已经初始化过,那肯定有.git文件夹
+           //初始化git库,相当于命令行的 git init
             Git.init().setDirectory(root).call();
         }
         Git git = Git.open(root); //打开git库
 
-                //好吧,随便写一个文件进去先
+        //好吧,随便写一个文件进去先
         File newFile = new File("gitme/"+System.currentTimeMillis()+".java");
         FileWriter fw = new FileWriter(newFile);
         fw.write(System.currentTimeMillis() + " ABC");
         fw.flush();
         fw.close();
 
-                //添加文件咯,相当于 git add .
+         //添加文件咯,相当于 git add .
         git.add().addFilepattern(".+").call();
 
-                //然后当然是提交啦,相当于 git commit
+         //然后当然是提交啦,相当于 git commit
         git.commit().setCommitter("wendal", "wendal1985@gmail.com").setMessage("Try jgit!").call();
 
         //接下来,我们看看log信息
